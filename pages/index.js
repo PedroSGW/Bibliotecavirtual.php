61502c673b2799f1122484c8f4801a6cd02b2fab
PedroSGW <pedrosergioorib@gmail.com>
2025-05-27 17:40:23 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import GoogleBooksSearch from '../components/GoogleBooksSearch';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Biblioteca Virtual</h1>
+      <GoogleBooksSearch />
+    </div>
+  );
+}
